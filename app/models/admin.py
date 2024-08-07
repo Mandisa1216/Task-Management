36e37a0e0451d3183c7d30700591dfb7b5069def
Mandisa1216 <146944361+Mandisa1216@users.noreply.github.com>
2024-08-07 12:19:12 -0700

    fixing my mongo to collect information

--- app/models/admin.py
+++ app/models/admin.py
@@ -3,7 +3,7 @@ from flask_bcrypt import Bcrypt
 
 
 class Get_admin:
-    def admin_collection(admin_data):
+    def create_new(admin_data):
         return mongo.db.user_collection.insert_one(admin_data)
         
     
