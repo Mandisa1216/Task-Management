99b32a9bb605c4e1e6c063db30fb8c641d83791d
Mandisa1216 <146944361+Mandisa1216@users.noreply.github.com>
2024-08-13 09:19:41 -0700

    mongodb connection

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config:
-    MONGO_URI = "mongodb://localhost:27017/signup"
+    MONGO_URI = "mongodb+srv://mandisandlovumd:PkAhlLF7kpd8jrPm@cluster0.8xi8iwb.mongodb.net/MYDATABASE"
     JWT_TOKEN_LOCATION = "headers"
     SECRET_KEY = "sdfghj"
